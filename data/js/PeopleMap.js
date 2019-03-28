c57b99e876f87ae9d57e0743dbc25a962985545b
t294gji4wu6 <48278823+t294gji4wu6@users.noreply.github.com>
2019-03-28 15:26:45 +0800

    student moving

--- data/js/PeopleMap.js
+++ data/js/PeopleMap.js
@@ -8,13 +8,15 @@ var PeopleMap = function(){
 	};
 
 	this.teacherPos = [
-		[10,10]
+		[1.5,3]
 	];
-	
-	
+	this.studentPos = [
+		[1.5,3]
+	];
 	this.load = function()
 	{
 		this.teacher=new Framework.Sprite(define.imagePath + 'teacher_left.png');
+		this.student=new Framework.Sprite(define.imagePath + 'treeStone.png');
 	};
 	
 	this.initialize = function()
@@ -24,7 +26,7 @@ var PeopleMap = function(){
 	
 	this.update = function()
 	{
-		
+
 	};
 	
 	this.draw = function(ctx)
@@ -34,10 +36,27 @@ var PeopleMap = function(){
 			x: this.position.x + (this.MW * 1) * this.teacherPos[0][0] + this.MW /2,
 			y: this.position.y + (this.MH * 1) * this.teacherPos[0][1] + this.MH/2
 		}
-		
+
 		this.teacher.position = picPosition;
 		this.teacher.draw(ctx)
-					
+		
+		
+		var studentPosition={
+			var x = this.student.offsetLeft
+			var y = this.student.offsetTop;
+			max=20;
+			min=-20;
+			x += Math.random() * (max - min) + min;
+			y += Math.random() * (max - min) + min;
+			this.studentPos.x = x;
+			this.studentPos.y = y;
+			
+			
+		}
+
+		this.student.position=studentPosition;
+		this.student.draw(ctx)
+
 	};
-	
+
 };
